52aaf8b7a4afd075c0d0263da07cac0d91a04d23
ocavue <ocavue@gmail.com>
2019-04-21 04:57:21 +0800

    good format

--- jsonschemax/__init__.py
+++ jsonschemax/__init__.py
@@ -1,15 +1,14 @@
 from jsonschemax.compilation import compile
 from jsonschemax.errors import (
-    JsonSchemaXError,
     InvalidInstanceError,
     InvalidSchemaError,
+    JsonSchemaXError,
 )
 from jsonschemax.keywords import draft7_keyword_map
 from jsonschemax.meta_schemas import draft7_meta_schema
 
 __all__ = (
     "compile",
-
     "draft7_meta_schema",
     "draft7_keyword_map",
     "JsonSchemaXError",
