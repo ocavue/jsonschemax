7f612189e8bf4fcebaf444b5563b0c05504e480d
ocavue <ocavue@gmail.com>
2019-04-21 04:52:33 +0800

    bad format

--- jsonschemax/__init__.py
+++ jsonschemax/__init__.py
@@ -1,14 +1,15 @@
 from jsonschemax.compilation import compile
 from jsonschemax.errors import (
     JsonSchemaXError,
-    InvalidSchemaError,
     InvalidInstanceError,
+    InvalidSchemaError,
 )
 from jsonschemax.keywords import draft7_keyword_map
 from jsonschemax.meta_schemas import draft7_meta_schema
 
 __all__ = (
     "compile",
+
     "draft7_meta_schema",
     "draft7_keyword_map",
     "JsonSchemaXError",
